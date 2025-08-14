6c3937359d2d89a7ebafd53e5720a7393eade476
Antonio Fdez. <antoniofernandezvara@gmail.com>
2025-08-14 12:54:43 +0200

    fix: website logo and favicon not embedded

--- web/embed.go
+++ web/embed.go
@@ -6,5 +6,5 @@ import (
 
 // Embed FS containing static assets. Paths are relative to this directory.
 //
-//go:embed static/dashboard.html static/style.css
+//go:embed static/dashboard.html static/style.css static/favicon.ico static/logo.svg
 var FS embed.FS
